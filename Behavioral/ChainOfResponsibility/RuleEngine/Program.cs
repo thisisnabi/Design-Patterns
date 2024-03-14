e16a449f442f6ef8d2c30c024f461b2e0b04f80b
Arshia_HZ <arshiahosseinzadeh@yahoo.com>
2024-03-14 15:34:00 +0330

    RuleEngine added in chain of responsibility

--- Behavioral/ChainOfResponsibility/RuleEngine/Program.cs
+++ Behavioral/ChainOfResponsibility/RuleEngine/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine;
+using Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine.Abstractions;
+using Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine.Rules;
+
+IRule rule1 = new Rule1();
+IRule rule2 = new Rule2();
+
+rule1.NextRule = rule2;
+
+// Execute the chain of rules against your input data.
+RuleHandler handler = new RuleHandler(rule1);
+handler.Execute(new object()); // Pass your input data here
---
+++ Behavioral/ChainOfResponsibility/RuleEngine/RuleHandler.cs
@@ -0,0 +1,14 @@
+﻿using Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine.Abstractions;
+
+namespace Thisisnabi.DesignPattern.Behavioral.ChainOfResponsibility.RuleEngine
+{
+    public class RuleHandler(IRule firstRule)
+    {
+        private readonly IRule _firstRule = firstRule;
+
+        public void Execute(object data)
+        {
+            _firstRule.ApplyRule(data);
+        }
+    }
+}
